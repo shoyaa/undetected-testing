40a30954e9898dd8366f6b2d314bcdbb4e8086f4
shoyaa <78690814+shoyaa@users.noreply.github.com>
2025-02-24 16:05:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ with SB(uc=True, test=True) as sb:
     sb.sleep(2)
     sb.uc_gui_handle_captcha()
     print("Success! Website did not detect SeleniumBase!!!")
+    selector = "body"
+    try:
+        text_content = sb.get_text(selector)
+        print(f"Text content of selector '{selector}':\n{text_content}")
+    except Exception as e:
+        print(f"Error getting text content: {e}")
